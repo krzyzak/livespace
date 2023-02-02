8829001f524949ac444355b5983eac00edc8a613
Michał Krzyżanowski <krzyzak@users.noreply.github.com>
2023-02-02 23:07:00 +0100

    First commit

---
+++ lib/livespace.rb
@@ -0,0 +1,10 @@
+require "livespace/version"
+require "dry-configurable"
+
+module Livespace
+  extend Dry::Configurable
+
+  setting :domain
+  setting :api_key
+  setting :api_secret
+end
---
+++ lib/livespace/version.rb
@@ -0,0 +1,3 @@
+module Livespace
+  VERSION = "0.1.0"
+end
